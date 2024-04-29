ce562c83c638a484247d0dd4f45c2da51c1f33ee
zhangkai <45248468+zhkai-ybwn@users.noreply.github.com>
2024-04-29 15:34:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oJfU26fQkAkucX2Sp2Zwb0sYWlsM',
+      id: 'oJfU26YPBV2W2p7iyKULu4Fff6Kg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 's7kyfZv42S7FqYYrkBes8pVWvPaoyKDCQmXEI65VDjw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
