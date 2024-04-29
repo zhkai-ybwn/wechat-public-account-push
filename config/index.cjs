5a99dd05a2cd4ee7570c4309b169cc7d621ae7eb
zhangkai <45248468+zhkai-ybwn@users.noreply.github.com>
2024-04-29 15:23:13 +0800

    Update index.cjs
    去除空格

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJfU26fQkAkucX2Sp2Zwb0sYWlsM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	s7kyfZv42S7FqYYrkBes8pVWvPaoyKDCQmXEI65VDjw',
+      useTemplateId: 's7kyfZv42S7FqYYrkBes8pVWvPaoyKDCQmXEI65VDjw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-30',
       festivals: [
